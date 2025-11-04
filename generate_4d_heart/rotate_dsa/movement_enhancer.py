b368766f25733d17da8fa9f71734e139929a4720
shi_jie <1592277253@qq.com>
2025-11-05 00:37:20 +0800

    - 新增 线性 冠脉运动增强

--- generate_4d_heart/rotate_dsa/movement_enhancer.py
+++ generate_4d_heart/rotate_dsa/movement_enhancer.py
@@ -46,7 +46,48 @@ class CoronaryBoundLV(MovementEnhancer):
         self.dilate_coronary_indices = cp.where(dilate_coronary_mask)
         self.smooth_indices = cp.where(smooth_mask)
 
-    
+    def __call__(self, dvf: torch.Tensor) -> torch.Tensor:
+        dvf_cp = cp.from_dlpack(tensor2dlpack(dvf.cuda()))
+        dvf_cp[:,:, *self.dilate_coronary_indices] = (
+            dvf_cp[:, :, *self.indices] * self.alpha
+            + dvf_cp[:, :, *self.dilate_coronary_indices] * (1 - self.alpha)
+        )
+        smoothed = cp.zeros_like(dvf_cp)
+        for i in range(3):
+            smoothed[:, i] = gaussian_filter(dvf_cp[:, i], sigma=2.0)
+        dvf_cp[:, :, *self.smooth_indices] = smoothed[:, :, *self.smooth_indices]
+        
+        dvf = dlpack2tensor(dvf_cp.toDlpack()).cpu()
+        del dvf_cp
+        return dvf
+
+class CoronaryBoundLVLinear(MovementEnhancer):
+    def __init__(
+        self, 
+        cavity_label: torch.Tensor, 
+        coronary_label: torch.Tensor,
+        enhance_weight_at_farthest_coroanry: float = 0.1
+    ) -> None:
+        lv = (cavity_label.to(torch.uint8) == LV_LABEL).squeeze()
+        lv_cp = cp.from_dlpack(tensor2dlpack(lv.cuda())).astype(cp.bool_)
+        
+        coronary_cp = cp.from_dlpack(tensor2dlpack(coronary_label.squeeze().cuda())).astype(cp.uint8)
+        dilate_coronary_mask = binary_dilation(coronary_cp, structure=cp.ones((9,9,9))).astype(cp.bool_)
+        smooth_mask = binary_dilation(dilate_coronary_mask).astype(cp.bool_)
+        
+        dist: cp.ndarray
+        indices: cp.ndarray
+        dist, indices = distance_transform_edt(~lv_cp, return_distances=True, return_indices=True) # type: ignore
+        self.indices = indices[:, dilate_coronary_mask]  # shape = (3, N), N is the total numpy of coronary voxels
+
+        dist_coronary_max = dist[dilate_coronary_mask].max()
+        alpha = (1 - enhance_weight_at_farthest_coroanry) / dist_coronary_max
+        self.alpha = cp.clip(1 - dist * alpha, 0.0, 1.0)
+        self.alpha = self.alpha[dilate_coronary_mask]
+        
+        self.dilate_coronary_indices = cp.where(dilate_coronary_mask)
+        self.smooth_indices = cp.where(smooth_mask)
+
     def __call__(self, dvf: torch.Tensor) -> torch.Tensor:
         dvf_cp = cp.from_dlpack(tensor2dlpack(dvf.cuda()))
         dvf_cp[:,:, *self.dilate_coronary_indices] = (
